95fbfb88dc059860f9cc1c260994d28e9dcdf2e1
arrguieb <166109485+arrguieb@users.noreply.github.com>
2026-04-05 16:27:10 -0700

    updated Main.py
    added a while loop for repeating input

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
-#amount
-amount = input("Enter amount: ")
-
-#transaction
-transaction_type = input("Is this income or expense?")
-
-#category
-category = input("Enter category (food, rent, etc.): ")
-
-#date
-date = input("Enter Date (YYYY-MM-DD): ")
-
-#prints everything
-print("Transaction:", amount, transaction_type, category, date)
-
-
-#transactions list
-#create an empty list to store the transactions
 transactions = []
 
+while True:
+        amount = input("Enter amount: ")
+        transaction_type = input("Is this income or expense?")
+        category = input("Enter category (food, rent, etc.): ")
+        date = input("Enter Date (YYYY-MM-DD): ")
 #add transactions to list
-transactions.append({
-    "amount": amount,
-    "type": transaction_type,
-    "category": category,
-    "date": date
-})
+        transactions.append({
+            "amount": amount,
+            "type": transaction_type,
+            "category": category,
+            "date": date
+        })
+
+        print("Transaction added")
+
+        #asks user if they want to add another transaction
+        cont = input("Add another? (y/n)")
+        if cont.lower() != "y":
+                break
 
 #prints all transactions
 print("All Transactions:", transactions)
+
